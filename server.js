ea75d6933e18f1937f4a5dcd5802a90d32f54bfd
AbhishekTiwari07 <abhishekt2024@gmail.com>
2021-08-04 17:01:20 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+const port = process.env.PORT || 2000
+
+app.listen(port, ()=>{
+    console.log(`Server up at port ${port}`)
+})
