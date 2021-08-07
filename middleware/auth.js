376b1aa12d35039a6732c4b9174d48b733cf3a78
AbhishekTiwari07 <abhishekt2024@gmail.com>
2021-08-07 13:26:50 +0530

    Added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('json-web-token')
+require('dotenv').config()
+
+const auth = async (req,res,next) =>{
+        const token = req.headers.authorization.split(" ")[1]
+        const decoded = jwt.verify(token,process.env.TOKEN_SECRET)
+    try{
+        const user = await User.findOne({ email : decoded})
+        if(!user){
+            throw new Error("error")
+        }
+        req.token = token
+        req.user = user
+    }
+    catch(e){
+        return res.send({
+            error : e.message,
+            message : "Please Authenticate"})
+    }
+    next()
+}
+
+module.exports = auth
